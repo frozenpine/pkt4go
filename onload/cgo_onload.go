2455daf69de6697361c6bbda5f3496243d8ebb9a
frozenpine <sonny.frozenpine@gmail.com>
2022-10-12 09:28:54 +0800

    完成efvi事件处理桥接C代码

--- onload/cgo_onload.go
+++ onload/cgo_onload.go
@@ -16,11 +16,23 @@ package onload
 #include <etherfabric/vi.h>
 #include <etherfabric/packedstream.h>
 #include <etherfabric/memreg.h>
+
+static inline int get_ef_event_poll(struct ef_vi *evq, ef_event *evs, int evs_len)
+{
+    return ef_eventq_poll(evq, evs, evs_len);
+}
+
+static inline u_int16_t get_ef_event_type(ef_event evt)
+{
+    return EF_EVENT_TYPE(evt);
+}
+
 */
 import "C"
 import (
 	"context"
 	"sync"
+	"unsafe"
 
 	"github.com/frozenpine/pkt4go"
 	"github.com/pkg/errors"
@@ -38,6 +50,7 @@ const (
 	defaultFlags        C.enum_ef_vi_flags = C.EF_VI_RX_PACKED_STREAM | C.EF_VI_RX_PS_BUF_SIZE_64K | C.EF_VI_RX_TIMESTAMPS
 	defaultHugePageSize int64              = 2 * 1024 * 1024
 	defaultPktBufferLen                    = 100
+	maxEvents                              = 16
 )
 
 var (
@@ -72,6 +85,38 @@ func (dev *Device) Release() {
 
 	dev.closeDH()
 }
+
+// EventType Possible types of events
+type EFEventType uint16
+
+const (
+	// Good data was received
+	EF_EVENT_TYPE_RX EFEventType = iota
+	// Packets have been sent
+	EF_EVENT_TYPE_TX
+	// Data received and buffer consumed, but something is wrong
+	EF_EVENT_TYPE_RX_DISCARD
+	// Transmit of packet failed
+	EF_EVENT_TYPE_TX_ERROR
+	// Received packet was truncated due to a lack of descriptors
+	EF_EVENT_TYPE_RX_NO_DESC_TRUNC
+	// Software generated event
+	EF_EVENT_TYPE_SW
+	// Event queue overflow
+	EF_EVENT_TYPE_OFLOW
+	// TX timestamp event
+	EF_EVENT_TYPE_TX_WITH_TIMESTAMP
+	// A batch of packets was received in a packed stream
+	EF_EVENT_TYPE_RX_PACKED_STREAM
+	// A batch of packets was received on a RX event merge vi
+	EF_EVENT_TYPE_RX_MULTI
+	// Packet has been transmitted via a "TX alternative"
+	EF_EVENT_TYPE_TX_ALT
+	// A batch of packets was received with error condition set
+	EF_EVENT_TYPE_RX_MULTI_DISCARD
+	// Event queue has been forcibly halted (hotplug, reset, etc.)
+	EF_EVENT_TYPE_RESET
+)
 
 func CreateHandler(src string) (*Device, error) {
 	if src == "" {
@@ -128,6 +173,13 @@ func createFilter(input string) *C.ef_filter_spec {
 	return &filter
 }
 
+// func getEventType(evt C.ef_event) EFEventType {
+// 	evtType := (EFEventType(evt[0]) << 8)
+// 	evtType |= EFEventType(evt[1])
+
+// 	return evtType
+// }
+
 func StartCapture(ctx context.Context, dev *Device, filter string, fn pkt4go.DataHandler) error {
 	if ctx == nil {
 		ctx = context.Background()
@@ -153,12 +205,22 @@ func StartCapture(ctx context.Context, dev *Device, filter string, fn pkt4go.Dat
 			done.Done()
 		}()
 
+		events := [maxEvents]C.ef_event{}
+
 		for {
 			select {
 			case <-ctx.Done():
 				return
 			default:
 				// TODO: packet handler
+				evtCount := int(C.get_ef_event_poll(&dev.vi, (*C.ef_event)(unsafe.Pointer(&events[0])), maxEvents))
+
+				for idx := 0; idx < evtCount; idx++ {
+					switch EFEventType(C.get_ef_event_type(events[idx])) {
+					case EF_EVENT_TYPE_RX_PACKED_STREAM:
+					default:
+					}
+				}
 			}
 		}
 	}()

d354bbc6632c0cf72e1399ed24eca4cd4e661029
frozenpine <sonny.frozenpine@gmail.com>
2023-08-25 14:31:59 +0800

    初步完成只读tcp socket封装

---
+++ socket/tcp.go
@@ -0,0 +1,132 @@
+package socket
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/frozenpine/pkt4go/core"
+)
+
+const (
+	defaultBuffSize = 4096
+)
+
+func DialTCP(ctx context.Context, front string, buffSize int, handler core.DataHandler) error {
+	if handler == nil {
+		return errors.New("data handler can not be nil")
+	}
+
+	if ctx == nil {
+		ctx = context.Background()
+	}
+
+	addr, err := net.ResolveTCPAddr("tcp", front)
+	if err != nil {
+		return err
+	}
+
+	conn, err := net.DialTCP("tcp", nil, addr)
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+
+	// raw, err := conn.SyscallConn()
+	// if err != nil {
+	// 	return err
+	// }
+
+	// raw.Control(func(fd uintptr) {
+	// 	err = syscall.SetNonblock(syscall.Handle(fd), true)
+	// })
+	// if err != nil {
+	// 	return err
+	// }
+
+	if buffSize <= 0 {
+		buffSize = defaultBuffSize
+	}
+
+	local := strings.Split(conn.LocalAddr().String(), ":")
+	localIP := net.ParseIP(strings.Trim(local[0], "[]"))
+	localPort, _ := strconv.Atoi(local[1])
+
+	remote := strings.Split(conn.RemoteAddr().String(), ":")
+	remoteIP := net.ParseIP(strings.Trim(remote[0], "[]"))
+	remotePort, _ := strconv.Atoi(remote[1])
+
+	cache := core.NewStreamCache()
+	session := core.Session{
+		Proto:   core.TCP,
+		SrcIP:   remoteIP,
+		SrcPort: remotePort,
+		DstIP:   localIP,
+		DstPort: localPort,
+	}
+
+	buff := make([]byte, buffSize)
+
+	for {
+		select {
+		case <-ctx.Done():
+			return nil
+		default:
+			var (
+				n   int
+				err error
+			)
+			// raw.Read(func(fd uintptr) (done bool) {
+			// 	n, err = syscall.Read(syscall.Handle(fd), buff)
+			// 	return true
+			// })
+
+			// if n == 0 {
+			// 	continue
+			// }
+			n, err = conn.Read(buff)
+			ts := time.Now()
+
+			if err != nil {
+				if errors.Is(err, io.EOF) {
+					err = nil
+				}
+
+				return err
+			}
+
+			buff = cache.Merge(buff[:n])
+			used, err := handler(&session, ts, buff)
+
+			if err != nil {
+				if errors.Is(err, io.EOF) {
+					return nil
+				}
+
+				return fmt.Errorf(
+					"[%s] %s data handler failed: %w",
+					ts, &session, err)
+			}
+
+			size := len(buff)
+
+			if used < 0 {
+				log.Printf("handler return size invalid: %d", used)
+				continue
+			}
+
+			if remain := cache.Append(buff[used:]); remain > 0 {
+				log.Printf(
+					"%s handler result: New[%d], Used[%d], Remain[%d]",
+					&session, size, used, remain,
+				)
+			}
+		}
+	}
+}
---
+++ socket/tcp_test.go
@@ -0,0 +1,50 @@
+package socket
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"testing"
+	"time"
+
+	"github.com/frozenpine/pkt4go/core"
+)
+
+func TestDialTCP(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
+
+	addr := "localhost:65500"
+
+	l, err := net.Listen("tcp", addr)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer l.Close()
+
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				c, err := l.Accept()
+
+				if err != nil {
+					t.Log(err)
+					continue
+				}
+
+				c.Write([]byte(fmt.Sprintf("hello %s", c.RemoteAddr())))
+				c.Close()
+			}
+		}
+	}()
+
+	if err := DialTCP(ctx, addr, 0, func(session *core.Session, ts time.Time, data []byte) (int, error) {
+		t.Log(session, ts, string(data))
+		return len(data), nil
+	}); err != nil {
+		t.Fatal(err)
+	}
+}
